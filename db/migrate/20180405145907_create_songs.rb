ac42a162341178ed7dd4c4db42c332bea3652876
Luisa Scavo <luisascavo@gmail.com>
2018-04-12 13:24:18 -0400

    add artist_id to songs

--- db/migrate/20180405145907_create_songs.rb
+++ db/migrate/20180405145907_create_songs.rb
@@ -3,6 +3,7 @@ class CreateSongs < ActiveRecord::Migration[5.1]
     create_table :songs do |t|
       t.string :title
       t.integer :rating
+      t.integer :artist_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180405145920) do
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.integer "rating"
+    t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
