f0c170e612e7100584a6876c0ff1700848037831
Luisa Scavo <luisascavo@gmail.com>
2018-04-12 12:48:09 -0400

    add post /artists

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#home'
+  post '/artists', to: 'artists#create', as: 'artists'
 
 end
